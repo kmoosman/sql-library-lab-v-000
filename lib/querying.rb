11d5380fc0aa8bcf850bdfd4aaadc7c4105218c2
Katie Moosman <moosman.katie@gmail.com>
2019-02-04 06:17:05 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,10 +22,12 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT subgenres.name, series.title
+  "SELECT authors.name, subgenres.name
   FROM series
   JOIN subgenres
-  ON subgenres.id = series.subgenre_id;"
+  ON subgenres.id = series.subgenre_id;
+  JOIN authors
+  ON subgenres.id = series.author_id;"
 end
 
 def select_series_title_with_most_human_characters
