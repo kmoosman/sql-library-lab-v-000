04bee3a804ef846004ef3f87f9748df42ee13f0f
Katie Moosman <moosman.katie@gmail.com>
2019-02-04 06:19:13 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,10 +24,10 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM series
-  JOIN subgenres
-  ON subgenres.id = series.subgenre_id
   JOIN authors
   ON subgenres.id = series.author_id
+  JOIN subgenres
+  ON subgenres.id = series.subgenre_id
   GROUP BY(authors.name);"
 end
 
