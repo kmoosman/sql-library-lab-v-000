f57151cebe96d02be481a24e1b939e536da5551d
Katie Moosman <moosman.katie@gmail.com>
2019-02-04 06:57:20 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,7 +32,12 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title
+  FROM characters
+  JOIN series
+  ON characters.series_id = series.id
+  WHERE characters.species = "human"
+  GROUP BY(series.title);"
 end
 
 def select_character_names_and_number_of_books_they_are_in
