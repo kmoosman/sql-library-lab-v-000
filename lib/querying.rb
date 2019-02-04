190a2bd61a15b0997fc4740bc8d74a351ca746d9
Katie Moosman <moosman.katie@gmail.com>
2019-02-04 06:19:37 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,7 +28,7 @@ def select_name_and_series_subgenres_of_authors
   ON subgenres.id = series.author_id
   JOIN subgenres
   ON subgenres.id = series.subgenre_id
-  GROUP BY(authors.name);"
+  GROUP BY(subgenres.name);"
 end
 
 def select_series_title_with_most_human_characters
